9beb3fc2154055b97f0566d7df5ee444b23a60f2
KrokoDog <luigi.grimaldi@hotmail.de>
2018-10-13 03:15:15 +0200

    enemy permutation

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float enemySpeed = 2f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newXPosition = this.transform.position.x - Time.deltaTime * enemySpeed;
+        this.transform.position = new Vector3(newXPosition, this.transform.position.y,0f);
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyContainer;
+    public float spawnTimer = 2.5f;
+    private bool enemyContainerSpawned = false;
+
+
+	// Use this for initialization
+	void Start () {
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+ 
+        if (!enemyContainerSpawned)
+        {
+            enemyContainerSpawned = true;
+            StartCoroutine(SpawnEnemy());
+        }
+
+    }
+    IEnumerator SpawnEnemy()
+    {
+        Instantiate(enemyContainer, this.transform.position, this.transform.rotation);
+        yield return new WaitForSeconds(spawnTimer);
+        enemyContainerSpawned = false;
+       
+    }
+
+
+}
