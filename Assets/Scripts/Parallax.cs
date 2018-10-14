a1b9843ba70f1a2b9707562ad1e12af62fdc1714
nerd100 <den.meyer1@web.de>
2018-10-14 13:32:27 +0200

    this is a funfair

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -17,24 +17,32 @@ public class Parallax : MonoBehaviour {
     public GameObject forgroundBude;
     public GameObject middlegroundBude;
 
+    private float forgroundSpawnPointX = 11.0f;
+    private float forgroundSpawnPointY = -4.0f;
+
+    private float middlegroundSpawnPointX = 11.0f;
+    private float middlegroundSpawnPointY = -1.5f;
+
     List<Sprite> budenSprites = new List<Sprite>();
     private Object[] budenImages;
+
+    Sprite randomSprite;
+
+    private bool spawnBudeGlobal = true;
+    private bool spawnBudeFor = true;
+    private bool spawnBudeMiddel = true;
     // Use this for initialization
 
     void Start () {
         layer1Speed = 1.0f;
         layer2Speed = 0.3f;
         layer3Speed = 0.1f;
-
-
         budenImages = Resources.LoadAll("Buden", typeof(Sprite));
 
         foreach (var t in budenImages)
         {
             budenSprites.Add((Sprite)t);
         }
-
-        print(budenSprites.Count);
 
     }
 
@@ -46,7 +54,50 @@ public class Parallax : MonoBehaviour {
             layer2.transform.position = new Vector3(layer2.transform.position.x - layer2Speed * Time.deltaTime, layer2.transform.position.y, layer2.transform.position.z);
             layer3.transform.position = new Vector3(layer3.transform.position.x - layer3Speed * Time.deltaTime, layer3.transform.position.y, layer3.transform.position.z);
 
+            if (spawnBudeGlobal) {
+                spawnBudeGlobal = false;
+                randomSprite = budenSprites[Random.Range(0, budenSprites.Count)];
 
+                if (randomSprite.name.Contains("for") && spawnBudeFor)
+                {
+                    spawnBudeFor = false;
+                    GameObject bude = Instantiate(forgroundBude, new Vector3(forgroundSpawnPointX, forgroundSpawnPointY, 0), transform.rotation);
+                    bude.GetComponent<SpriteRenderer>().sprite = randomSprite;
+                    //bude.transform.position = new Vector3(bude.transform.position.x - layer1Speed * Time.deltaTime, bude.transform.position.y - layer1Speed * Time.deltaTime, bude.transform.position.z);
+                    //bude.transform.parent = layer1.transform;
+
+                    StartCoroutine(spawnBudenTimeoutForGround());
+                }
+                else if(spawnBudeMiddel) 
+                {
+                    spawnBudeMiddel = false;
+                    GameObject bude = Instantiate(middlegroundBude, new Vector3(middlegroundSpawnPointX, middlegroundSpawnPointY, 0), transform.rotation);
+                    bude.GetComponent<SpriteRenderer>().sprite = randomSprite;
+                    //bude.transform.position = new Vector3(bude.transform.position.x - layer2Speed * Time.deltaTime, bude.transform.position.y - layer2Speed * Time.deltaTime, bude.transform.position.z);
+                    //bude.transform.parent = layer2.transform;
+
+                    StartCoroutine(spawnBudenTimeoutMiddelGround());
+                }
+
+                StartCoroutine(spawnBudenTimeoutGlobal());
+            }
         }
+    }
+
+    IEnumerator spawnBudenTimeoutGlobal()
+    {
+        yield return new WaitForSeconds(5);
+        spawnBudeGlobal = true;
+    }
+
+    IEnumerator spawnBudenTimeoutForGround()
+    {
+        yield return new WaitForSeconds(5);
+        spawnBudeFor = true;
+    }
+    IEnumerator spawnBudenTimeoutMiddelGround()
+    {
+        yield return new WaitForSeconds(10);
+        spawnBudeMiddel = true;
     }
 }
